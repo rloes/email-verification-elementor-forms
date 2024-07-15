a0d285ee41ce2339668d51eaf3c1baff75b85deb
rloes <robin.loeseke@gmail.com>
2024-07-16 01:09:04 +0200

    Fix: Escaping issue

--- includes/field-templates/classic.php
+++ includes/field-templates/classic.php
@@ -5,7 +5,7 @@
 if (!defined('ABSPATH')) exit;
 $normal_text = apply_filters('evef/classic/normal_text', __('Send email again', 'email-verification-elementor-forms'));
 /* translators: %s: Countdown, until sent again is available again. */
-$success_text = apply_filters('evef/classic/success_text', sprintf(__('Email was sent again, check spam or try again in %s seconds.', 'email-verification-elementor-forms'), "<span class='timer'></span>"));
+$success_text = apply_filters('evef/classic/success_text', __('Email was sent again, check spam or try again in %s seconds.', 'email-verification-elementor-forms'));
 $error_text = apply_filters('evef/classic/error_text', __('An error occured while sending the mail', 'email-verification-elementor-forms'));
 $loader_html = apply_filters('evef/classic/loader_html', '<div class="evef-loader"></div>');
 ?>
@@ -13,7 +13,7 @@ $loader_html = apply_filters('evef/classic/loader_html', '<div class="evef-loade
 <input <?= $form->get_render_attribute_string('input' . $item_index) ?> >
 <span role="button" class="send-code-again" tabindex="0" aria-live="polite">
     <span class="normal"><?= esc_html($normal_text) ?></span>
-    <span class="success" style="display:none;" aria-hidden="true"><?= esc_html($success_text) ?></span>
+    <span class="success" style="display:none;" aria-hidden="true"><?= sprintf(esc_html($success_text),"<span class='timer'></span>") ?></span>
     <span class="error" style="display:none;" aria-hidden="true"><?= esc_html($error_text) ?></span>
     <?= wp_kses_post($loader_html) ?>
 </span>
